684c9ca6c56c485ff5900caa54b65562e29c8bbd
James Batt <getjamesbatt@gmail.com>
2019-12-10 12:24:20 +1100

    added prettier

--- website/src/Store.ts
+++ website/src/Store.ts
@@ -2,10 +2,10 @@ import { store } from 'react-easy-state';
 
 export interface IDevice {
   name: string;
-	publicKey: string;
-	endpoint: string;
-	address: string;
-	dns: string;
+  publicKey: string;
+  endpoint: string;
+  address: string;
+  dns: string;
   createdAt: string;
   serverPublicKey: string;
   // TODO: these fields on backend
