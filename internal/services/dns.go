83d0b943c8b64d014d71efc39853ccb694d5f420
James Batt <getjamesbatt@gmail.com>
2020-01-27 21:26:04 +1100

    dns caching

--- internal/services/dns.go
+++ internal/services/dns.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"net"
 	"runtime/debug"
+	"time"
 
 	"github.com/miekg/dns"
+	"github.com/patrickmn/go-cache"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
@@ -13,22 +15,36 @@ import (
 type DNSServer struct {
 	server   *dns.Server
 	client   *dns.Client
+	cache    *cache.Cache
 	upstream []string
 }
 
 func NewDNSServer(upstream []string) (*DNSServer, error) {
 	logrus.Infof("starting dns server")
-	server := &dns.Server{Addr: "0.0.0.0:53", Net: "udp"}
-	client := &dns.Client{
-		SingleInflight: true,
+
+	if len(upstream) == 0 {
+		upstream = []string{"1.1.1.1"}
 	}
-	dnsServer := &DNSServer{server, client, upstream}
-	server.Handler = dnsServer
+
+	dnsServer := &DNSServer{
+		server: &dns.Server{
+			Addr: "0.0.0.0:53",
+			Net:  "udp",
+		},
+		client: &dns.Client{
+			SingleInflight: true,
+		},
+		cache:    cache.New(10*time.Minute, 10*time.Minute),
+		upstream: upstream,
+	}
+	dnsServer.server.Handler = dnsServer
+
 	go func() {
-		if err := server.ListenAndServe(); err != nil {
+		if err := dnsServer.server.ListenAndServe(); err != nil {
 			logrus.Error(errors.Wrap(err, "failed to start dns server"))
 		}
 	}()
+
 	return dnsServer, nil
 }
 
@@ -52,9 +68,8 @@ func (d *DNSServer) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 		}
 		m, err := d.Lookup(r)
 		if err != nil {
-			logrus.Errorf("failed lookup record %s with error: %s\n", r, err.Error())
-			m.SetReply(r)
-			w.WriteMsg(m)
+			logrus.Errorf("failed lookup record with error: %s\n%s", err.Error(), r)
+			dns.HandleFailed(w, r)
 			return
 		}
 		m.SetReply(r)
@@ -64,17 +79,39 @@ func (d *DNSServer) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 }
 
 func (d *DNSServer) Lookup(m *dns.Msg) (*dns.Msg, error) {
-	// TODO: add support for caching
+	key := makekey(m)
+
+	// check the cache first
+	if item, found := d.cache.Get(key); found {
+		if logrus.GetLevel() == logrus.DebugLevel {
+			logrus.Debugf("dns cache hit %s", prettyPrintMsg(m))
+		}
+		return item.(*dns.Msg), nil
+	}
+
+	// fallback to upstream exchange
 	response, _, err := d.client.Exchange(m, net.JoinHostPort(d.upstream[0], "53"))
 	if err != nil {
 		return nil, err
 	}
+
+	if len(response.Answer) > 0 {
+		ttl := time.Duration(response.Answer[0].Header().Ttl) * time.Second
+		logrus.Debugf("caching dns response for %v seconds", ttl)
+		d.cache.Set(key, response, ttl)
+	}
+
 	return response, nil
+}
+
+func makekey(m *dns.Msg) string {
+	q := m.Question[0]
+	return fmt.Sprintf("%s:%d:%d", q.Name, q.Qtype, q.Qclass)
 }
 
 func prettyPrintMsg(m *dns.Msg) string {
 	if len(m.Question) > 0 {
-		return fmt.Sprintf("dns query for: %s", m.Question[0].Name)
+		return fmt.Sprintf("dns query for: %s", makekey(m))
 	}
 	return m.String()
 }
